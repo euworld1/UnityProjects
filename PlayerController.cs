e916ae9e98af4063d15347e9a76b614a73399f47
euworld1 <euworld1@gmail.com>
2017-07-25 13:09:50 -0600

    Working rollerball

---
+++ PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}	
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveJump = Input.GetAxis ("Up");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveJump * 50, moveVertical);
+		rb.AddForce (movement);
+
+	}
+
+}
